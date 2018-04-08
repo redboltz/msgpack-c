8402571ac6355edb55f26d443715dfad732b9ac9
Takatoshi Kondo <redboltz@gmail.com>
2018-04-08 17:26:43 +0900

    Unpack offset update.

--- include/msgpack/v3/unpack.hpp
+++ include/msgpack/v3/unpack.hpp
@@ -29,16 +29,13 @@ inline msgpack::object_handle unpack(
     msgpack::object obj;
     msgpack::unique_ptr<msgpack::zone> z(new msgpack::zone);
     referenced = false;
-    std::size_t noff = off;
     parse_return ret = detail::unpack_imp(
-        data, len, noff, *z, obj, referenced, f, user_data, limit);
+        data, len, off, *z, obj, referenced, f, user_data, limit);
 
     switch(ret) {
     case msgpack::PARSE_SUCCESS:
-        off = noff;
         return msgpack::object_handle(obj, msgpack::move(z));
     case msgpack::PARSE_EXTRA_BYTES:
-        off = noff;
         return msgpack::object_handle(obj, msgpack::move(z));
     default:
         break;
@@ -83,18 +80,15 @@ inline void unpack(
     msgpack::object obj;
     msgpack::unique_ptr<msgpack::zone> z(new msgpack::zone);
     referenced = false;
-    std::size_t noff = off;
     parse_return ret = detail::unpack_imp(
-        data, len, noff, *z, obj, referenced, f, user_data, limit);
+        data, len, off, *z, obj, referenced, f, user_data, limit);
 
     switch(ret) {
     case msgpack::PARSE_SUCCESS:
-        off = noff;
         result.set(obj);
         result.zone() = msgpack::move(z);
         return;
     case msgpack::PARSE_EXTRA_BYTES:
-        off = noff;
         result.set(obj);
         result.zone() = msgpack::move(z);
         return;
@@ -142,17 +136,14 @@ inline msgpack::object unpack(
     msgpack::unpack_limit const& limit)
 {
     msgpack::object obj;
-    std::size_t noff = off;
     referenced = false;
     parse_return ret = detail::unpack_imp(
-        data, len, noff, z, obj, referenced, f, user_data, limit);
+        data, len, off, z, obj, referenced, f, user_data, limit);
 
     switch(ret) {
     case msgpack::PARSE_SUCCESS:
-        off = noff;
         return obj;
     case msgpack::PARSE_EXTRA_BYTES:
-        off = noff;
         return obj;
     default:
         break;
